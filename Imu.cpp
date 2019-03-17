66ade44779ded322235c3a895a70436b8e4c9c51
mhuertas <manuel.huertas.lopez@gmail.com>
2019-03-17 07:53:12 +0000

    The magnetometer is read from I2C bus

--- Imu.cpp
+++ Imu.cpp
@@ -1,12 +1,15 @@
 #include "Imu.h"
 
+#define addr 0x0D //I2C Address for The HMC5883
+
 Imu::Imu()
 {
-    
+    init();
 }
 
 boolean Imu::init() 
 {
+
     Wire.begin();
     bool ret;
     
@@ -19,9 +22,11 @@ boolean Imu::init()
     ret = gyroscope.testConnection();
     if(!ret)
         return false;
-  
     
-    magnetometer.init();
+    Wire.beginTransmission(addr); //start talking
+    Wire.write(0x09); // Set the Register
+    Wire.write(0x1D); // Tell the HMC5883 to Continuously Measure
+    Wire.endTransmission();
 
     return true;
 }
@@ -36,7 +41,7 @@ geometry_msgs::Vector3 Imu::readAccelerometer()
     accel.x = ax * (double) ACCEL_SCALE * G_TO_ACCEL;
     accel.y = ay * (double) ACCEL_SCALE * G_TO_ACCEL;
     accel.z = az * (double) ACCEL_SCALE * G_TO_ACCEL;
-
+    
     return accel;
 }
 
@@ -53,22 +58,33 @@ geometry_msgs::Vector3 Imu::readGyroscope()
     gyro.z = gz * (double) GYRO_SCALE * DEG_TO_RAD;
 
     return gyro;
-
 }
 
 geometry_msgs::Vector3 Imu::readMagnetometer()
 {
-        geometry_msgs::Vector3 mag;
+    geometry_msgs::Vector3 mag;
     int16_t mx, my, mz;
 
-    mx = my = mz = 0;
-
-    //magnetometer.getHeading(&mx, &my, &mz); TODO
+    //Tell the HMC what regist to begin writing data into
+    Wire.beginTransmission(addr);
+    Wire.write(0x00); //start with register 3.
+    Wire.endTransmission();
+  
+ 
+    //Read the data.. 2 bytes for each axis.. 6 total bytes
+    Wire.requestFrom(addr, 6);
+    if(6<=Wire.available()){
+        mx = Wire.read()<<8; //MSB  x 
+        mx |= Wire.read(); //LSB  x
+        mz = Wire.read()<<8; //MSB  z
+        mz |= Wire.read(); //LSB z
+        my = Wire.read()<<8; //MSB y
+        my |= Wire.read(); //LSB y
+    }
 
     mag.x = mx * (double) MAG_SCALE * UTESLA_TO_TESLA;
     mag.y = my * (double) MAG_SCALE * UTESLA_TO_TESLA;
     mag.z = mz * (double) MAG_SCALE * UTESLA_TO_TESLA;
 
     return mag;
-
 }
